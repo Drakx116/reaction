63b0c830bb727955f3deddfcdfa6610f2034cae2
Drakx116 <alexandre2.guyot@gmail.com>
2021-01-29 17:13:11 +0100

    Reworked routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import 'bulma/css/bulma.css'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import 'bulma/css/bulma.css';
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
-  return (
-      <div className="container">
-        <h1 className="title"> Reaction </h1>
-      </div>
-  );
+    return (
+      <Router>
+          <Switch>
+              <Route unique path="/" component={ Dashboard } />
+          </Switch>
+      </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+      <div className="container">
+          <h1 className="title"> Star Wars Legacy </h1>
+      </div>
+    );
+}
+
+export default Dashboard;
