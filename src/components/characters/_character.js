7f96aa802195e14d4aaaf979fdf14c1c4e9ad4cf
Drakx116 <alexandre2.guyot@gmail.com>
2021-03-12 17:24:23 +0100

    Add character single page LIKE and DISLIKE buttons and link them to the store like actions

--- src/components/characters/_character.js
+++ src/components/characters/_character.js
@@ -1,4 +1,15 @@
-const Character = ({ data }) => {
+import store from "../../redux/store";
+import actions from "../../redux/actions/like";
+
+const Character = ({ data, id }) => {
+    const like = () => {
+        store.dispatch(actions.like(id));
+    }
+
+    const dislike = () => {
+        store.dispatch(actions.dislike(id));
+    };
+
     return(
         <div className="character">
             <span><b> Name </b> { data.name }</span><br/>
@@ -6,7 +17,10 @@ const Character = ({ data }) => {
             <span><b> Birth Year </b> { data.birth_year }</span><br/>
             <span><b> Height </b> { data.height }</span><br/>
             <span><b> Mass </b> { data.mass }</span><br/>
-            <span><b> Hair Color </b> { data.hair_color }</span>
+            <span><b> Hair Color </b> { data.hair_color }</span><br/><br/>
+
+            <button onClick={ like }> LIKE </button>
+            <button onClick={ dislike }> DISLIKE </button>
         </div>
     )
 }
